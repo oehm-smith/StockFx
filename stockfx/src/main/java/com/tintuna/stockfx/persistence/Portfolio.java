4cc50aa5ec2a6f6f382a926f8ed46f8300ac2b6c
Brooke Smith <brooke@tintuna.com>
2013-02-23 23:07:45 +1100

    I originally wrote the persistence / Portfolio.java @Entity as a JavaFx model with Properties.  It didn't work as Properties aren't serializable, so I reverted and planned to use a DTO to load between the model and database.  I realised something and moved the @Id from the 'long id' to the 'getId()' and then suddenly the persistence not happening on the property but the getters that return a string.  So this works now and don't need to have separate model and persistencee entities.  However I always planned to have some separation so that I can quite easily convert to using a WebApp with web services to deliver the data instead of it coming from a local Db.
    What I think I'll do here is retain the Model and Persistencee, and put a backend implementation specific DTO (based on an Interface which I already have - its PortfolioLoader) which for the current arrangement with a local DB will have the two properties (ie. one for each at each end) mirror the other like properties do.  I'm not sure if you can do this so will do some investigation.
    
    Anyway, this commit is having the new persistence/Portfolio.java @Entity that is composed of properties.

--- stockfx/src/main/java/com/tintuna/stockfx/persistence/Portfolio.java
+++ stockfx/src/main/java/com/tintuna/stockfx/persistence/Portfolio.java
@@ -1,32 +1,102 @@
 package com.tintuna.stockfx.persistence;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+//@Entity
+//public class Portfolio {
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	private long id;
+//
+//	private String name;
+//	private String type;
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	public String getType() {
+//		return type;
+//	}
+//
+//	public void setType(String type) {
+//		this.type = type;
+//	}
+//}
+
 @Entity
 public class Portfolio {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 
-	private String name;
-	private String type;
+	private StringProperty name;
+	private StringProperty type;
+
+	public Portfolio() {
+		
+	}
+	
+	public Portfolio(String name, String type) {
+		setName(name);
+		setType(type);
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
 
 	public String getName() {
-		return name;
+		if (name == null) {
+			return "";
+		}
+		return name.get();
 	}
 
 	public void setName(String name) {
-		this.name = name;
+		getNameProperty().set(name);
+	}
+
+	public StringProperty getNameProperty() {
+		if (name == null) {
+			name = new SimpleStringProperty();
+		}
+		return name;
 	}
 
 	public String getType() {
-		return type;
+		if (type == null) {
+			return "";
+		}
+		return type.get();
 	}
 
 	public void setType(String type) {
-		this.type = type;
+		getTypeProperty().set(type);
+	}
+
+	public StringProperty getTypeProperty() {
+		if (type == null) {
+			type = new SimpleStringProperty();
+		}
+		return type;
+	}
+	
+	public String toString() {
+		return getName()+" : "+getType();
 	}
 }
